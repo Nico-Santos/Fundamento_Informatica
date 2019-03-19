80366d03662dd988d524b599c8f03f07669fcc58
Nico-Santos <santos.m.nicolas@gmail.com>
2019-03-18 21:44:09 -0300

    Ejercicios

---
+++ Ej2.py
@@ -0,0 +1,15 @@
+#Ejercicio 2.a
+r = 12 * 4 + 4 * 5
+print("Ej 2.a:", r)
+
+#2.b
+r = (12 * (1 - 5) + 4) * 3
+print("Ej 2.b:", r)
+
+#2.c
+r = 12 * 1 - 5 + 4 * 3
+print("Ej 2.c:", r)
+
+#2.d
+r = (17 - 2) / 5
+print("Ej 2.d:", r)
