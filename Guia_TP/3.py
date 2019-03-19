6ab3e988c8c11874cee04b3b61bfc9938ec884eb
Nico-Santos <santos.m.nicolas@gmail.com>
2019-03-18 22:31:06 -0300

    Agregado parte b

--- Guia_TP/3.py
+++ Guia_TP/3.py
@@ -10,3 +10,15 @@ if (radio == 0):
 	print("Error, no es un circulo")
 else:
 	sup_circulo(radio)
+
+#Ej. 3b
+
+def perimetro_circunsferencia(radio):
+	print("Perimetro de la circunsferencia: ", radio)
+
+radio = float(input("Ingrese radio: "))
+
+if (radio == 0):
+	print("Error, no es una circunsferencia")
+else:
+	perimetro_circunsferencia(radio)
