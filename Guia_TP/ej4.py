70038db653164ac281de6d9bd9920bc297df8224
Nico-Santos <santos.m.nicolas@gmail.com>
2019-03-19 22:10:23 -0300

    Ej 4 agregado (Preguntar a profe)

---
+++ Guia_TP/ej4.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+
+#Ej.  4
+# Todos los años tienen 365 dias
+def anos_en_dias(edad):
+    print("%d año/s = %d dias" % (edad, edad * 365))
+
+edad = int(input("Ingrese su edad: "))
+anos_en_dias(edad)
