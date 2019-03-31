d9cf83140f0f5b9da709b22f6adffe88624c8cd1
NicoSantos <santos.m.nicolas@gmail.com>
2019-03-31 11:49:48 -0300

    Tp3

---
+++ Guia_TP/TP3/ej12.py
@@ -0,0 +1,21 @@
+#Verificar si la fecha ingresada (dd/mm/aa) es valida
+print("Verificacion de la fecha que introdusca\n")
+
+anio = int(input("Ingrese un año: "))
+
+if( anio >= 1900):
+    mes = int(input("Ingrese un mes del año: "))
+    if( mes >= 1 and mes <= 12):
+        dia = int(input("Ingrese un dia del año: "))
+        if( dia >= 1 and dia <= 31):
+            if( mes == 2 and dia <= 29):
+                if( (anio % 4) == 0 and (anio % 400) == 0 and dia == 29):
+                    print("La fecha que ingreso es valida")
+                elif( (anio % 4) != 0 and (anio % 400) != 0 and dia == 29):
+                    print("La fecha que ingreso no es valida")
+          
+            
+
+
+
+                
---
+++ Guia_TP/TP3/ej13.py
@@ -0,0 +1,31 @@
+#Calcula sueldo de un empleado en base a sueldo y antiguedad
+
+print("Calculo del salario de un empleado\n")
+
+sueldo_basico = float(input("Sueldo basico del empleado: "))
+if(sueldo_basico <= 0):
+    print("Dato no valido")
+else:
+    jubilacion = sueldo_basico * 0.11
+    obra_social = sueldo_basico * 0.03
+    sindicato = sueldo_basico * 0.03
+    antiguedad = int(input("Antigüedad del empleado: "))
+
+    if(antiguedad < 0):
+        print("Dato no valido")
+    else:
+        estado_civil = input("Estado civil (s o c): ")
+        if(estado_civil == 's'):
+            sueldo = sueldo_basico + antiguedad * 0.05 
+            print("Sueldo neto: $", sueldo - jubilacion - obra_social - sindicato)
+
+        elif(estado_civil == 'c'):
+            sueldo = sueldo_basico + antiguedad * 0.07
+            print("Sueldo neto: $", sueldo - jubilacion - obra_social - sindicato)
+
+        else:
+            print("Dato no valido")
+
+
+
+
