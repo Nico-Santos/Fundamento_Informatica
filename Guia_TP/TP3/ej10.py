93c67821628cc5fe5ec158c233b165cbcb2d0a3a
NicoSantos <santos.m.nicolas@gmail.com>
2019-04-03 20:43:11 -0300

    Cambio ej10

--- Guia_TP/TP3/ej10.py
+++ Guia_TP/TP3/ej10.py
@@ -1,13 +1,14 @@
 #Devuelve que tipo de triangulo es, segun lados
 def tipo_triangulo(h, c1, c2):
-    if( (h**2 == c1**2 + c2**2) and h != 0 and c1 != 0 and c2 != 0):
-        print("Forma un triangulo rectangulo")
-    elif( h**2 > c1**2 + c2**2):
-        print("Forma un triangulo obtusangulo")
-    elif( h**2 < c1**2 + c2**2):
-        print("Forma un triangulo acutangulo")
+    if(h < c1 + c2 and c1 < h + c2 and c2 < h + c1):
+        print("no es un triangulo")
     else:
-        print("No es un triangulo")
+        if( (h**2 == c1**2 + c2**2) and h != 0 and c1 != 0 and c2 != 0):
+            print("Forma un triangulo rectangulo")
+        elif( h**2 > c1**2 + c2**2):
+            print("Forma un triangulo obtusangulo")
+        elif( h**2 < c1**2 + c2**2):
+            print("Forma un triangulo acutangulo")
 
 h = float(input("Ingrese hipotenusa: "))
 c1 = float(input("Ingrese cateto: "))
