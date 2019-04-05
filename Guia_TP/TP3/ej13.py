8771e9b40a112406b06c0ffb9e2b3a529f84055b
Nico-Santos <45690787+Nico-Santos@users.noreply.github.com>
2019-04-05 11:46:43 -0300

    Update ej13.py

--- Guia_TP/TP3/ej13.py
+++ Guia_TP/TP3/ej13.py
@@ -17,17 +17,17 @@ else:
         estado_civil = input("Estado civil: soltero(s) o casado(c)): ")
         if(estado_civil == 's'):
             sueldo = sueldo_basico + antiguedad * 0.05 
-            print("\nDescuento jubilacion: $", -jubilacion)
-            print("Descuento obra social: $",-obra_social)
-            print("Descuento sindicato: $", -sindicato)
+            print("\nDescuento jubilacion: -$", jubilacion)
+            print("Descuento obra social: -$", obra_social)
+            print("Descuento sindicato: -$", sindicato)
             print("Sueldo basico: $", sueldo_basico)
             print("Sueldo neto: $", sueldo - jubilacion - obra_social - sindicato)
 
         elif(estado_civil == 'c'):
             sueldo = sueldo_basico + antiguedad * 0.07
-            print("\nDescuento jubilacion: $", -jubilacion)
-            print("Descuento obra social: $",-obra_social)
-            print("Descuento sindicato: $", -sindicato)
+            print("\nDescuento jubilacion: -$", jubilacion)
+            print("Descuento obra social: -$", obra_social)
+            print("Descuento sindicato: -$", sindicato)
             print("Sueldo basico: $", sueldo_basico)
             print("Sueldo neto: $", sueldo - jubilacion - obra_social - sindicato)
 
