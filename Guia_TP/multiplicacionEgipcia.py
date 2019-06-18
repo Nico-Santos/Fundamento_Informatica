195b36085495e6a5b8b47c8d58096debc44a4d9d
Nico-Santos <45690787+Nico-Santos@users.noreply.github.com>
2019-06-18 18:07:49 -0300

    Update multiplicacionEgipcia.py

--- Guia_TP/multiplicacionEgipcia.py
+++ Guia_TP/multiplicacionEgipcia.py
@@ -23,3 +23,33 @@ while(suma != limite):
     else:
         j -= 1
 print(suma * inicial)
+
+'''
+exponencial = []
+doble = []
+inicial = int(input("Inicial: "))
+limite = int(input("Limite: "))
+exp = 0
+suma = 0
+du = 1
+
+while(suma < limite):
+    exponencial.append(2**exp)
+    suma += (2**exp)
+    exp += 1
+    doble.append(inicial*du)
+    du *= 2
+print(exponencial)
+print(doble)
+suma = exponencial[len(exponencial)-1]
+j = len(exponencial) - 2
+res = doble[len(doble)-1]
+while(suma != limite):    
+    if((exponencial[j] + suma) <= limite):
+        suma += exponencial[j]
+        res += doble[j]
+        j -= 1
+    else:
+        j -= 1
+print(res)
+'''
